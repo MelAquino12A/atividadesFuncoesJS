a2bf1b6d0489e717fa0525dd34e46a785ca2899d
unknown <melissa.aquino.andrade@gmail.com>
2025-05-05 00:53:09 -0300

    resolução do exercício gasto de combustível

---
+++ pages-gastoCombustivel/gastoCombustivel.js
@@ -0,0 +1,7 @@
+let tempo = Number(prompt("Digite o tempo gasto na viagem (em horas):"));
+let velocidade = Number(prompt("Digite a velocidade média (em km/h):"));
+
+let distancia = tempo * velocidade;
+let litrosNecessarios = distancia / 12;
+
+console.log(litrosNecessarios.toFixed(3));
