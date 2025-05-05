6430a86f906f5e2191c60a2c77dcbffebc2819b6
unknown <melissa.aquino.andrade@gmail.com>
2025-05-05 00:17:22 -0300

    resolução do exercício extremamente básico

---
+++ pages-extremamenteBasico/extemamenteBasico.js
@@ -0,0 +1,13 @@
+function somar(valor1, valor2) {
+    return valor1 + valor2
+ }
+
+ let teste1 = somar(10, 9)
+ let teste2 = somar(-10, 4)
+ let teste3 = somar(15, -7)
+
+ console.log(`
+    X1 = ${teste1}
+    X2 = ${teste2}
+    X3 = ${teste3}
+`)
